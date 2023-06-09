0eb2ca2eb0588eacd3fbac723ecd6b482fcb4738
lsgro <me@luigisgro.com>
2023-06-09 18:17:05 +0200

    Added some doc

--- src/service.rs
+++ src/service.rs
@@ -11,7 +11,7 @@ use crate::exchange::ConnectedBookUpdateProvider;
 
 use crate::orderbook::{Summary, Level};
 
-
+/// Conversion between internal exchange price level to protobuf type.
 impl From<&ExchangeLevel> for Level {
     fn from(value: &ExchangeLevel) -> Self {
         Level {
@@ -22,12 +22,27 @@ impl From<&ExchangeLevel> for Level {
     }
 }
 
+/// Service connecting two Binance and Bitstamp to stream a consolidated book
 pub struct BookSummaryService {
+    /// A merged stream of Binance and Bitmap trading book snapshots.
+    ///
+    /// # Returns
+    ///
+    /// A stream of [BookUpdate](BookUpdate) objects
     book_update_stream: Pin<Box<Select<ConnectedBookUpdateProvider, ConnectedBookUpdateProvider>>>,
     aggregate_book: AggregateBook,
 }
 
 impl BookSummaryService {
+    /// Return a new instance of the service.
+    ///
+    /// # Arguments
+    ///
+    /// * `product` - A [currency pair](CurrencyPair) used to query the exchanges
+    ///
+    /// # Returns
+    ///
+    /// An instance of [BookSummaryService](BookSummaryService)
     pub async fn new(product: CurrencyPair) -> Self {
         let bitstamp_provider = make_bitstamp_provider(&product);
         let binance_provider = make_binance_provider(&product);
@@ -38,12 +53,22 @@ impl BookSummaryService {
         Self { book_update_stream, aggregate_book }
     }
 
+    /// Disconnect from both exchanges, it consumes the service
     pub async fn disconnect(self) {
         let (stream1, stream2) = Pin::into_inner(self.book_update_stream).into_inner();
         let _ = stream1.disconnect().await;
         let _ = stream2.disconnect().await;
     }
 
+    /// Extract a protobuf message [Summary](Summary) from the current state of an aggregate book (static method).
+    ///
+    /// # Arguments
+    ///
+    /// * `aggregate_book` - A reference to an (aggregate book)[AggregateBook]
+    ///
+    /// # Returns
+    ///
+    /// An instance of [BookSummaryService](BookSummaryService)
     fn make_summary(aggregate_book: &AggregateBook) -> Summary {
         let best_bids = aggregate_book.best_bids();
         let best_asks = aggregate_book.best_asks();
@@ -57,6 +82,13 @@ impl BookSummaryService {
         Summary { spread, bids, asks }
     }
 
+    /// Apply a [book update](BookUpdate) object if available, and return an up-to-date [Summary](Summary) object.
+    ///
+    /// # Arguments
+    ///
+    /// * `product` - An optional [BookUpdate](BookUpdate)
+    ///
+    /// # Return
     fn update_and_make_summary(&mut self, maybe_book_update: Option<BookUpdate>) -> Summary {
         if let Some(book_update) = maybe_book_update {
             self.aggregate_book.update(book_update);
@@ -65,6 +97,7 @@ impl BookSummaryService {
     }
 }
 
+/// (Stream)[Stream] implementation for the service that outputs protobuf (Summary)[Summary] objects.
 impl Stream for BookSummaryService {
     type Item = Summary;
 

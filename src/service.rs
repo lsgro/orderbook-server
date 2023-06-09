1af1ea79b4461b37ee804df1ec7e2abbe7fb8d16
lsgro <me@luigisgro.com>
2023-06-09 14:04:06 +0200

    Refactoring: leaner trait implementation

--- src/service.rs
+++ src/service.rs
@@ -23,8 +23,8 @@ impl From<&ExchangeLevel> for Level {
 }
 
 pub struct BookSummaryService {
+    book_update_stream: Pin<Box<Select<ConnectedBookUpdateProvider, ConnectedBookUpdateProvider>>>,
     aggregate_book: AggregateBook,
-    stream: Pin<Box<Select<ConnectedBookUpdateProvider, ConnectedBookUpdateProvider>>>,
 }
 
 impl BookSummaryService {
@@ -33,13 +33,13 @@ impl BookSummaryService {
         let binance_provider = make_binance_provider(&product);
         let connected_bitstamp_provider = bitstamp_provider.connect().await.expect("Connection to Bitstamp failed");
         let connected_binance_provider = binance_provider.connect().await.expect("Connection to Binance failed");
-        let stream= Box::pin(select(connected_bitstamp_provider, connected_binance_provider));
+        let book_update_stream= Box::pin(select(connected_bitstamp_provider, connected_binance_provider));
         let aggregate_book = AggregateBook::new(NUM_LEVELS);
-        Self { aggregate_book, stream }
+        Self { book_update_stream, aggregate_book }
     }
 
     pub async fn disconnect(self) {
-        let (stream1, stream2) = Pin::into_inner(self.stream).into_inner();
+        let (stream1, stream2) = Pin::into_inner(self.book_update_stream).into_inner();
         let _ = stream1.disconnect().await;
         let _ = stream2.disconnect().await;
     }
@@ -56,20 +56,21 @@ impl BookSummaryService {
         };
         Summary { spread, bids, asks }
     }
+
+    fn update_and_make_summary(&mut self, maybe_book_update: Option<BookUpdate>) -> Summary {
+        if let Some(book_update) = maybe_book_update {
+            self.aggregate_book.update(book_update);
+        }
+        Self::make_summary(&self.aggregate_book)
+    }
 }
 
 impl Stream for BookSummaryService {
     type Item = Summary;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        self.stream.as_mut().poll_next(cx).map(
-            move |maybe_book_update| {
-                if let Some(book_update) = maybe_book_update {
-                    self.aggregate_book.update(book_update);
-                }
-                let summary: Summary = Self::make_summary(&self.aggregate_book);
-                Some(summary)
-            }
+        self.book_update_stream.as_mut().poll_next(cx).map( |maybe_book_update|
+            Some(self.update_and_make_summary(maybe_book_update))
         )
     }
 }
